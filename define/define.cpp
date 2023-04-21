fde9be73ad4b722da6611956fd083c7ee2b0accc
ElRomario <hellishassailant007@gmail.com>
2023-04-21 18:36:06 +0300

    shitsabout to be done

---
+++ define/define.cpp
@@ -0,0 +1,10 @@
+﻿#define TRIANGLE_PERIMETR(A, B, C)(A + B + C)
+#define HALF_PERIMETR(A, B, C)((A+B+C)/2)
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << HALF_PERIMETR(4, 5, 6);
+}
+
